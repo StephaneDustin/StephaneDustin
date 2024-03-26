504fcd122f7270207b19a0b16ec0c7ab23cb22e1
StephaneDustin <stephane.taveres@protonmail.com>
2024-03-26 22:20:37 +0100

    Tinder edit

--- Jedha/Tinder_Project/Tinder_project.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib as mp
-import seaborn as sns
-df = pd.read_csv ("https://full-stack-assets.s3.eu-west-3.amazonaws.com/M03-EDA/Speed+Dating+Data.csv")
-print (df.head(5))
